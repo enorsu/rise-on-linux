3fd4d50cd5068f2cf375f693fb9afeb94b0ff5fd
SotaNorsu <pelaaja538@gmail.com>
2024-10-07 16:06:47 +0300

    credit

--- scripts/Start Linux.sh
+++ scripts/Start Linux.sh
@@ -1,3 +1,5 @@
 mkdir save
 cd save
 ../files/dependants/linux/jdk-22.0.2+9-jre/bin/java -noverify -Xms4096m -XX:+DisableAttachMechanism -Xmx4g -Djava.library.path=../files/dependants/linux/natives -cp "../files/Standalone.jar:../files/Libraries.jar:lwjgl.jar:lwjgl_util.jar" Start
+# This script is made by SotaNorsu
+# Contact me on discord("enorsu") if you have any problems
