c27170fd42149191993a23c619a3ef9cb73603ad
SotaNorsu <pelaaja538@gmail.com>
2024-10-07 16:36:16 +0300

    Auto-update part II

--- scripts/Start Linux.sh
+++ scripts/Start Linux.sh
@@ -4,10 +4,13 @@ if [ "$1" = "--update" ]; then
     echo "Updating Script!"
     rm "Linux Start.sh"
     wget -N https://github.com/enorsu/rise/raw/refs/heads/main/scripts/Start%20Linux.sh
+    sh "Start Linux.sh"
+else
+    mkdir save
+    cd save
+    ../files/dependants/linux/jdk-22.0.2+9-jre/bin/java -noverify -Xms4096m -XX:+DisableAttachMechanism -Xmx4g -Djava.library.path=../files/dependants/linux/natives -cp "../files/Standalone.jar:../files/Libraries.jar:lwjgl.jar:lwjgl_util.jar" Start
 fi
 
-mkdir save
-cd save
-../files/dependants/linux/jdk-22.0.2+9-jre/bin/java -noverify -Xms4096m -XX:+DisableAttachMechanism -Xmx4g -Djava.library.path=../files/dependants/linux/natives -cp "../files/Standalone.jar:../files/Libraries.jar:lwjgl.jar:lwjgl_util.jar" Start
+
 # This script is made by SotaNorsu
 # Contact me on discord("enorsu") if you have any problems
