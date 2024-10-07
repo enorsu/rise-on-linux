05d594a296f9d868bc50d6a4c13103196d1d685a
SotaNorsu <pelaaja538@gmail.com>
2024-10-07 16:29:16 +0300

    auto update

--- scripts/Start Linux.sh
+++ scripts/Start Linux.sh
@@ -1,4 +1,11 @@
 
+
+if [ "$1" = "--update" ]; then
+    echo "Updating Script!"
+    rm "Linux Start.sh"
+    wget -N https://github.com/enorsu/rise/raw/refs/heads/main/scripts/Start%20Linux.sh
+fi
+
 mkdir save
 cd save
 ../files/dependants/linux/jdk-22.0.2+9-jre/bin/java -noverify -Xms4096m -XX:+DisableAttachMechanism -Xmx4g -Djava.library.path=../files/dependants/linux/natives -cp "../files/Standalone.jar:../files/Libraries.jar:lwjgl.jar:lwjgl_util.jar" Start
