ab8498b388ab4b20c8598086c85a802cee5d8f09
SotaNorsu <pelaaja538@gmail.com>
2024-10-07 16:12:26 +0300

    delete

--- scripts/Start Linux.sh
+++ scripts/Start Linux.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+
 mkdir save
 cd save
 ../files/dependants/linux/jdk-22.0.2+9-jre/bin/java -noverify -Xms4096m -XX:+DisableAttachMechanism -Xmx4g -Djava.library.path=../files/dependants/linux/natives -cp "../files/Standalone.jar:../files/Libraries.jar:lwjgl.jar:lwjgl_util.jar" Start
