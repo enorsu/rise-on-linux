ca7d8196ec610349a872e358f93b80fa320d086f
SotaNorsu <pelaaja538@gmail.com>
2025-01-02 15:39:05 +0200

    added support for updater + you cant join servers rise fucked it ups

--- scripts/Start Linux Old.sh
+++ scripts/Start Linux Old.sh
@@ -6,14 +6,8 @@ if [ "$1" = "--update" ]; then
     wget -N https://github.com/enorsu/rise/raw/refs/heads/main/scripts/Start%20Linux.sh
     sh "Linux Start.sh"
 else
-    cd files
-
-    ./dependants/linux/jdk-22.0.2+9-jre/bin/java -jar "Launcher.jar"
-
-    cd ..
     mkdir save
-    cd ./save
-
+    cd save
     ../files/dependants/linux/jdk-22.0.2+9-jre/bin/java -noverify -Xms4096m -XX:+DisableAttachMechanism -Xmx4g -Djava.library.path=../files/dependants/linux/natives -cp "../files/Standalone.jar:../files/Libraries.jar:lwjgl.jar:lwjgl_util.jar" Start
 fi
 
